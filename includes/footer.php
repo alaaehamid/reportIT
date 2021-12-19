d3b4edb1b4321f586c5836bfd83a17196461f2a9
alaa hamid <65719378+alaaehamid@users.noreply.github.com>
2021-12-19 11:33:59 +0000

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+
+    <!-- MDB -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <script type="text/javascript" src="js/one.js"></script>
+    <!-- Custom scripts -->
+    <script type="text/javascript"></script>
+    
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,143 @@
+<?php include('includes/header.php'); ?>
+<?php include('includes/navbar.php'); ?>
+
+
+<?php include('includes/slider.php'); ?>
+
+
+<div class="container py-5">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body ">
+                    
+                    <p class="w3-serif">What is sexual harassment? <br></p>
+                    <p class="w3-sans-serif">Sexual harassment is any kind of unwanted behaviour of a sexual nature that makes you feel humiliated or intimidated, violates your dignity or creates a hostile environment.  
+
+  
+
+You don’t need to have previously objected to someone's behaviour for it to be considered unwanted.  
+
+ 
+
+Sexual harassment can include:  
+
+sexual comments or jokes  
+
+physical behaviour, including unwelcome sexual advances, touching and various forms of sexual assault  
+
+displaying pictures, photos, or drawings of a sexual nature  
+
+sending emails or texts with a sexual content  
+
+ 
+
+Sexual harassment can lead to anxiety, depression, and other problems such as difficulties sleeping.   
+
+ 
+
+No matter what the situation is, sexual harassment is unacceptable and not your fault.   
+
+ 
+
+If you have been subjected to unwanted sexual behaviour, you can find further advice here…. (Then provide link to advice section?)  
+
+</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h3>The first step to reach out for support will be different for everyone.
+
+                        You can speak to someone over the phone or chat to someone online by contacting the services
+                        listed below – you might want to contact one which feels right for you and your needs.
+
+                    </h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h3>Hello 1 </h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="container py-5">
+    <div class="row">
+        <div class="col-md-4">
+            <button type="button" class="btn-lg btn-primary px-5" name="report_button" style="sty.css">Report
+                Anonymously</button>
+        </div>
+        <div class="col-md-4">
+            <button type="button" class="btn-lg btn-primary px-5 mid">Report With Details</button>
+        </div>
+        <div class="col-md-4">
+            <button type="button" class="btn-lg btn-primary px-5 mid">Share you experiance</button>
+        </div>
+    </div>
+
+
+</div>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h3>Info</h3>
+                    <a href="First link">First linkFirst link</a><br>
+
+                    <a href="First link">First link</a><br>
+                    <a href="First link">First link</a><br>
+                    <a href="First link">First link</a><br>
+                    <a href="First link">First link</a><br>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h3>Info</h3>
+                    <a href="First link">First linkFirst link</a><br>
+
+                    <a href="First link">First link</a><br>
+                    <a href="First link">First link</a><br>
+                    <a href="First link">First link</a><br>
+                    <a href="First link">First link</a><br>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h3>Info</h3>
+                    <a href="First link">First linkFirst link</a><br>
+
+                    <a href="First link">First link</a><br>
+                    <a href="First link">First link</a><br>
+                    <a href="First link">First link</a><br>
+                    <a href="First link">First link</a><br>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<?php include('includes/footer.php'); ?>
